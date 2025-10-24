9c8b528ae46b67cb71382e7f4bc39b86d8efa35c
wenuam <david.koch@wenuam.fr>
2025-10-24 16:30:54 +0200

    25.0.0_250814

--- include/win32/jni_md.h
+++ include/win32/jni_md.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2024, Oracle and/or its affiliates. All rights reserved.
  * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  *
  *
@@ -30,7 +30,6 @@
   #define JNIEXPORT __declspec(dllexport)
 #endif
 #define JNIIMPORT __declspec(dllimport)
-#define JNICALL __stdcall
 
 typedef int jint;
 typedef long long jlong;
